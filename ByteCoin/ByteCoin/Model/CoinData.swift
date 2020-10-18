965e19d3410e3cc8c798a22710fcce626a383406
raishasan <72363979+raishasan@users.noreply.github.com>
2020-10-18 14:17:52 -0700

    Add files via upload

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Rais Hasan on 2020-10-18.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
